3ff22a09a34529ce2b5185858e3e2a2c056bfaf1
Mansour Cheyo <mcheyo.mc@gmail.com>
2019-10-15 14:05:09 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,4 @@ second_number = 3
 sum = first_number + second_number# Add your variables here
 difference = first_number - second_number
 product = first_number * second_number
-variable = first_number / second_number
+quotient = first_number / second_number
